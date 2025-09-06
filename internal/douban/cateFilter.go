80b7e57146270b5ad88baa63f6c869613c8e2714
smartutu <45462981+smartutu@users.noreply.github.com>
2025-09-06 22:50:49 +0800

    Update cateFilter.go

--- internal/douban/cateFilter.go
+++ internal/douban/cateFilter.go
@@ -4,8 +4,6 @@ import (
 	"TVHelper/global"
 	"TVHelper/internal/common"
 	"encoding/base64"
-	"encoding/json"
-	"math"
 	"strconv"
 	"strings"
 
@@ -42,54 +40,6 @@ func CateFilter(cateType, ext, pg, douban string) (cateFilterResult common.Resul
 			"start":       strconv.Itoa((curPage - 1) * count),
 			"count":       strconv.Itoa(count),
 		}).Get(path)
-	case "1hot_gaia":
-		area := GJsonGetDefault(gjson.GetBytes(extDecodeBytes, "area"), "全部")
-		sort := GJsonGetDefault(gjson.GetBytes(extDecodeBytes, "sort"), "recommend")
-		resp, err = global.DouBanClient.R().SetQueryParams(map[string]string{
-			"area":  area,
-			"sort":  sort,
-			"start": strconv.Itoa((curPage - 1) * count),
-			"count": strconv.Itoa(count),
-		}).Get("/movie/hot_gaia")
-	case "2tv_hot", "3show_hot":
-		sType := GJsonGetDefault(gjson.GetBytes(extDecodeBytes, "type"), cateType[1:])
-		path := strings.Join([]string{"/subject_collection/", sType, "/items"}, "")
-		resp, err = global.DouBanClient.R().SetQueryParams(map[string]string{
-			"start": strconv.Itoa((curPage - 1) * count),
-			"count": strconv.Itoa(count),
-		}).Get(path)
-	case "6rank_list_movie", "7rank_list_tv":
-		id := GJsonGetDefault(gjson.GetBytes(extDecodeBytes, "榜单"),
-			strings.Join([]string{strings.Split(cateType, "_")[2],
-				"real_time_hotest"},
-				"_"))
-		path := strings.Join([]string{"/subject_collection/", id, "/items"}, "")
-		resp, err = global.DouBanClient.R().SetQueryParams(map[string]string{
-			"start": strconv.Itoa((curPage - 1) * count),
-			"count": strconv.Itoa(count),
-		}).Get(path)
-	default:
-		sort := ""
-		tags := ""
-		selectedCategories := map[string]string{
-			"类型": gjson.GetBytes(extDecodeBytes, "类型").String(),
-			"地区": gjson.GetBytes(extDecodeBytes, "地区").String(),
-		}
-		sort = GJsonGetDefault(gjson.GetBytes(extDecodeBytes, "sort"), "T")
-		tags = GJsonArrayToStringExcept(gjson.GetBytes(extDecodeBytes, `@values`), sort, ",")
-		if cateType[1:] == "tv" {
-			selectedCategories["形式"] = gjson.GetBytes(extDecodeBytes, "形式").String()
-		}
-		selectedCategoriesJson, _ := json.Marshal(selectedCategories)
-		path := strings.Join([]string{"/", cateType[1:], "/recommend"}, "")
-		resp, err = global.DouBanClient.R().SetQueryParams(map[string]string{
-			"tags":                tags,
-			"sort":                sort,
-			"refresh":             "0",
-			"selected_categories": string(selectedCategoriesJson),
-			"start":               strconv.Itoa((curPage - 1) * count),
-			"count":               strconv.Itoa(count),
-		}).Get(path)
 	}
 	if err != nil {
 		return
@@ -109,16 +59,7 @@ func CateFilter(cateType, ext, pg, douban string) (cateFilterResult common.Resul
 	}
 
 	lists := make([]common.Vod, 0)
-	path := ""
-
-	switch cateType {
-	case "2tv_hot", "3show_hot", "6rank_list_movie", "7rank_list_tv":
-		path = "subject_collection_items"
-	case "0interests":
-		path = "interests.#.subject"
-	default:
-		path = "items"
-	}
+	path := "interests.#.subject"
 
 	gjson.Get(resp.String(), path).ForEach(func(_, v gjson.Result) bool {
 		itemType := v.Get("type").String()
@@ -129,7 +70,6 @@ func CateFilter(cateType, ext, pg, douban string) (cateFilterResult common.Resul
 				VodId: strings.Join([]string{"msearch:", itemType, "__", v.Get("id").String()},
 					""),
 				VodName: GJsonGetDefault(v.Get("title"), "暂不支持展示"),
-				//VodPic:     strings.Join([]string{v.Get("pic.normal").String(), "@User-Agent=com.douban.frodo"}, ""),
 				VodPic:     v.Get("pic.normal").String(),
 				VodRemarks: strings.TrimSpace(strings.Join([]string{rating, honorInfos}, " ")),
 			})
